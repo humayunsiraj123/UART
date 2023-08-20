c66b9d3ba0efa90fffdcfcf26df37eb202e23b97
Humayun Sheikh <humayunsiraj0@gmail.com>
2023-08-20 19:43:04 +0500

    Create baud_generator.sv

---
+++ baud_generator.sv
@@ -0,0 +1,29 @@
+
+module baud_generator #(parameter BAUD_RATE=2400) (
+	input  clk ,
+	input  srst,
+	output clk_t
+);
+
+	parameter FREQ = 1000000; //1Mhz;
+	parameter baud = FREQ/BAUD_RATE;
+
+	logic [31:0] count = 0;
+
+	always_ff @(posedge clk ) begin
+		if(srst)
+			count <= 0;
+		else if(count>=baud) 			
+			count <= 0;
+		else 
+			count <= count + 1  ;
+		end
+	end
+
+
+
+	always_comb begin : proc_
+		clk_t = count == baud;
+
+	end
+endmodule : baud_generator
